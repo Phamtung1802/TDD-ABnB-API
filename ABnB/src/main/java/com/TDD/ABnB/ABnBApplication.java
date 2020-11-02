e2ba2a2ff0e34a41639b011b48dd3e92c419d8a4
Phamtung1802 <phamtung.0218@gmail.com>
2020-11-02 14:34:16 +0700

    first commit

---
+++ ABnB/src/main/java/com/TDD/ABnB/ABnBApplication.java
@@ -0,0 +1,13 @@
+package com.TDD.ABnB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ABnBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ABnBApplication.class, args);
+	}
+
+}
---
+++ ABnB/src/test/java/com/TDD/ABnB/ABnBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TDD.ABnB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ABnBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
