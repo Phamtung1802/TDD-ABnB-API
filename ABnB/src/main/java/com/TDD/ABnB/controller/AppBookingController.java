a05f856d9e3db29bb89ed2a52445aafbbf34e7e6
dungnguyen1709 <56040352+dungnguyen1709@users.noreply.github.com>
2020-11-04 11:12:23 +0700

    cap nhat lai crud

--- ABnB/src/main/java/com/TDD/ABnB/controller/AppBookingController.java
+++ ABnB/src/main/java/com/TDD/ABnB/controller/AppBookingController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping({"/bookings"})
+@RequestMapping("/bookings")
 public class AppBookingController {
 
     @Autowired
