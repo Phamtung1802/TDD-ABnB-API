ca2e7fab9b9938768a0dee9b01d59d1a8d689d96
Phamtung1802 <phamtung.0218@gmail.com>
2020-11-04 08:01:53 +0700

    update review repo

--- ABnB/src/main/java/com/TDD/ABnB/repositories/AppReviewRepository.java
+++ ABnB/src/main/java/com/TDD/ABnB/repositories/AppReviewRepository.java
@@ -1,9 +1,10 @@
 package com.TDD.ABnB.repositories;
 
 import com.TDD.ABnB.models.AppInvoice;
+import com.TDD.ABnB.models.AppReview;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AppReviewRepository extends PagingAndSortingRepository<AppInvoice,Long> {
+public interface AppReviewRepository extends PagingAndSortingRepository<AppReview,Long> {
 }
