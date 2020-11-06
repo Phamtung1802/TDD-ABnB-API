56c077715700bca88a9a51840ab3583d41722a57
dungnguyen1709 <56040352+dungnguyen1709@users.noreply.github.com>
2020-11-06 08:25:02 +0700

    dang ki api

--- ABnB/src/main/java/com/TDD/ABnB/ABnBApplication.java
+++ ABnB/src/main/java/com/TDD/ABnB/ABnBApplication.java
@@ -4,17 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class ABnBApplication {
+
 	@Bean
 	public BCryptPasswordEncoder bCryptPasswordEncoder() {    return new BCryptPasswordEncoder(); }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ABnBApplication.class, args);
 
-
 	}
 
-
 }
+
--- ABnB/src/main/java/com/TDD/ABnB/exceptions/DuplilcateUserException.java
+++ ABnB/src/main/java/com/TDD/ABnB/exceptions/DuplilcateUserException.java
@@ -6,7 +6,7 @@ import javax.validation.ConstraintViolation;
 import javax.validation.ConstraintViolationException;
 import java.util.Set;
 
-public class DuplilcateUserException extends Exception{
+public class    DuplilcateUserException extends Exception{
     public DuplilcateUserException(String message) {
         super(message);
     }
